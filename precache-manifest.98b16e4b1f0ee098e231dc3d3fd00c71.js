a37404fe1f13984308de52cc177de1b9bb0cd52a
Kalibek Turgumbayev <kalibek_turgumbayev@epam.com>
2019-04-08 17:19:37 +0600

    'update'

---
+++ precache-manifest.98b16e4b1f0ee098e231dc3d3fd00c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f320bc545c62d3991d4d",
+    "url": "/static/js/main.8e3ba597.chunk.js"
+  },
+  {
+    "revision": "770298ef1a6180b83aae",
+    "url": "/static/js/2.e86706b1.chunk.js"
+  },
+  {
+    "revision": "f320bc545c62d3991d4d",
+    "url": "/static/css/main.6962363c.chunk.css"
+  },
+  {
+    "revision": "5e52712f4cc4a2a75c5eed254a9669f4",
+    "url": "/index.html"
+  }
+];
